18538b6e0bd33317b23dbaeda8cef457db400ca5
Nittinthecoder <93333941+Nittinthecoder@users.noreply.github.com>
2024-03-05 11:31:55 +0530

    add new packages and configed tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/Header.js",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        agro: {
+          primarygreen: '#58c004',
+          secondarygreen: '#69c61d',
+          basket: '#ace082'
+        },
       },
     },
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
 };
